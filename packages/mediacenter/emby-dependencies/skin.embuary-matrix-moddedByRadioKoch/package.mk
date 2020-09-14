0cf5adb693e0d81f436fe3312e5db6953557d2b1
Melf Stöcken <melf.stoecken@googlemail.com>
2020-09-14 07:07:23 +0000

    copy skin folder to writable system

--- packages/mediacenter/emby-dependencies/skin.embuary-matrix-moddedByRadioKoch/package.mk
+++ packages/mediacenter/emby-dependencies/skin.embuary-matrix-moddedByRadioKoch/package.mk
@@ -1,4 +1,4 @@
-PKG_NAME="kodi-theme-Embuary"
+PKG_NAME="skin.embuary-matrix-moddedByRadioKoch"
 PKG_VERSION="0.0.1"
 PKG_SHA256="48e1a558955d8878d06910e353eee8353e62b0ccddee5f97401f84466d708429"
 PKG_LICENSE="CC by-nc-nd 4.0"
@@ -15,12 +15,12 @@ makeinstall_target() {
   	cp -a $PKG_BUILD/* $INSTALL/usr/share/kodi/addons/$PKG_NAME
 
 	echo '##########################################################'
-	echo '#################### ADD DEPENDENCIES ####################'
+	echo '## ADD DEPENDENCIES'
 	echo '##########################################################'
   	mkdir $PKG_BUILD/dependencies
 
 	###############################################################################################
-	##################################### Kodi Binary Addons ######################################
+	## Kodi Binary Addons
 	###############################################################################################
 
 	###############################################################################################
@@ -216,8 +216,69 @@ makeinstall_target() {
   	rm $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
   	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/* $INSTALL/usr/share/kodi/addons/$EMBY_DEPENDENCY_NAME
 
+	####################################### Resources      ########################################
 	###############################################################################################
-	####################################### Embuary Addons ########################################
+	EMBY_DEPENDENCY_NAME="resource.language.de_de"
+	EMBY_DEPENDENCY_VERSION="9.0.24"
+	EMBY_DEPENDENCY_GIT="http://mirrors.kodi.tv/addons/matrix/resource.language.de_de"
+	EMBY_DEPENDENCY_FILENAME=$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION
+
+  	wget $EMBY_DEPENDENCY_GIT/$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION.zip -O $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+	mkdir $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	unzip -q $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip -d $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	rm $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+  	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/* $INSTALL/usr/share/kodi/addons/$EMBY_DEPENDENCY_NAME
+
+	###############################################################################################
+	EMBY_DEPENDENCY_NAME="resource.language.fr_fr"
+	EMBY_DEPENDENCY_VERSION="9.0.27"
+	EMBY_DEPENDENCY_GIT="http://mirrors.kodi.tv/addons/matrix/resource.language.fr_fr"
+	EMBY_DEPENDENCY_FILENAME=$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION
+
+  	wget $EMBY_DEPENDENCY_GIT/$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION.zip -O $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+	mkdir $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	unzip -q $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip -d $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	rm $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+  	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/* $INSTALL/usr/share/kodi/addons/$EMBY_DEPENDENCY_NAME
+
+	###############################################################################################
+	EMBY_DEPENDENCY_NAME="resource.language.es_es"
+	EMBY_DEPENDENCY_VERSION="9.0.26"
+	EMBY_DEPENDENCY_GIT="http://mirrors.kodi.tv/addons/matrix/resource.language.es_es"
+	EMBY_DEPENDENCY_FILENAME=$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION
+
+  	wget $EMBY_DEPENDENCY_GIT/$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION.zip -O $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+	mkdir $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	unzip -q $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip -d $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	rm $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+  	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/* $INSTALL/usr/share/kodi/addons/$EMBY_DEPENDENCY_NAME
+
+	###############################################################################################
+	EMBY_DEPENDENCY_NAME="resource.language.ar_sa"
+	EMBY_DEPENDENCY_VERSION="9.0.8"
+	EMBY_DEPENDENCY_GIT="http://mirrors.kodi.tv/addons/matrix/resource.language.ar_sa"
+	EMBY_DEPENDENCY_FILENAME=$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION
+
+  	wget $EMBY_DEPENDENCY_GIT/$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION.zip -O $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+	mkdir $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	unzip -q $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip -d $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	rm $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+  	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/* $INSTALL/usr/share/kodi/addons/$EMBY_DEPENDENCY_NAME
+
+	###############################################################################################
+	EMBY_DEPENDENCY_NAME="resource.language.ru_ru"
+	EMBY_DEPENDENCY_VERSION="9.0.20"
+	EMBY_DEPENDENCY_GIT="http://mirrors.kodi.tv/addons/matrix/resource.language.ru_ru"
+	EMBY_DEPENDENCY_FILENAME=$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION
+
+  	wget $EMBY_DEPENDENCY_GIT/$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION.zip -O $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+	mkdir $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	unzip -q $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip -d $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+  	rm $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.zip
+  	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/* $INSTALL/usr/share/kodi/addons/$EMBY_DEPENDENCY_NAME
+
+	###############################################################################################
+	## Embuary Addons
 	###############################################################################################
 
 	###############################################################################################
@@ -268,59 +329,76 @@ makeinstall_target() {
   	rm $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.tar.gz
   	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/* $INSTALL/usr/share/kodi/addons/$EMBY_DEPENDENCY_NAME
 
-	echo '#################################### List installed addons  ####################################'
-	ls -l $INSTALL/usr/share/kodi/addons
-	echo '#################################### -+ END +- ####################################'
-
-
-
 	###############################################################################################
-	######################################### Add Config ##########################################
+	## Add Boatsman Config
 	###############################################################################################
 	EMBY_DEPENDENCY_NAME="boatsman-config"
-	EMBY_DEPENDENCY_VERSION="0.0.1"
+	EMBY_DEPENDENCY_VERSION="9c11e2d1a7a50d3a6378783cf3f4b81d8ae7167c"
 	EMBY_DEPENDENCY_GIT="https://github.com/Melf11/boatsman-config"
 	EMBY_DEPENDENCY_FILENAME=$EMBY_DEPENDENCY_NAME-$EMBY_DEPENDENCY_VERSION
 
   	wget $EMBY_DEPENDENCY_GIT/archive/$EMBY_DEPENDENCY_VERSION.tar.gz -O $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.tar.gz
-	mkdir $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
+	  mkdir $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
   	tar -xf $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.tar.gz -C $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME
   	rm $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_FILENAME.tar.gz
 
-  	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/* $INSTALL/usr/share/kodi/addons/$EMBY_DEPENDENCY_NAME
+	cp -a $PKG_BUILD/dependencies/$EMBY_DEPENDENCY_NAME/$EMBY_DEPENDENCY_FILENAME/kodi/* $INSTALL/usr/share/kodi
+
+
+
+	echo '##############################################################'
+	echo '## List installed addons'
+	echo '##############################################################'
+	ls -l $INSTALL/usr/share/kodi/addons
+	echo '#################################### -+ END +- ####################################'
 }
 
 post_install() {
 
-  mkdir -p $INSTALL/usr/lib/systemd/system
-  cp $PKG_DIR/scripts/kodi-theme-Embuary $INSTALL/usr/bin
+	mkdir -p $INSTALL/usr/lib/systemd/system
+	cp $PKG_DIR/scripts/copy-emby-dependencies $INSTALL/usr/bin
 
-  enable_service kodi-theme-Embuary.service
+	## Enable Kodi Copy Service for Boot
+	###############################################################################################
+	enable_service copy-emby-dependencies.service
 
-  # update addon manifest / enable addon in Kodi
-#  ADDON_MANIFEST=$INSTALL/usr/share/kodi/system/addon-manifest.xml
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "kodi-theme-Embuary" $ADDON_MANIFEST
+	## Enabling Addons from first boot
+	###############################################################################################
+	ADDON_MANIFEST=$INSTALL/usr/share/kodi/system/addon-manifest.xml
 
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.skinshortcuts" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.unidecode" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.simpleeval" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.requests" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.certifi" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.chardet" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.idna" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.urllib3" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.arrow" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.simplecache" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.routing" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.simplejson" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.autocompletion" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "plugin.program.autocompletion" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.dateutil" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.six" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "kodi-theme-Embuary" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "plugin.video.embycon" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.embuary.helper" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.embuary.info" $ADDON_MANIFEST
-#  xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "resource.uisounds.embuary" $ADDON_MANIFEST
+	## Languages
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "resource.language.de_de" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "resource.language.fr_fr" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "resource.language.es_es" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "resource.language.ar_sa" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "resource.language.ru_ru" $ADDON_MANIFEST
+
+	## Embuary Theme
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "skin.embuary-matrix-moddedByRadioKoch" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.embuary.helper" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.embuary.info" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "resource.uisounds.embuary" $ADDON_MANIFEST
+
+	## EmbyCon Plugin
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "plugin.video.embycon" $ADDON_MANIFEST
+
+	## Embuary Theme Dependencies
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.autocompletion" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "plugin.program.autocompletion" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.arrow" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.unidecode" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.simpleeval" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.requests" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.certifi" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.chardet" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.idna" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.urllib3" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.simplecache" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.routing" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.simplejson" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.dateutil" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.module.six" $ADDON_MANIFEST
+	xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.skinshortcuts" $ADDON_MANIFEST
+
 
 }
